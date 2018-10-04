737b588fb14aef0d0294bc42998b72b99faf35cc
cjb <cjb>
2018-10-04 21:49:46 +0100

    zedboard: reformat FMC connector

--- migen/build/platforms/zedboard.py
+++ migen/build/platforms/zedboard.py
@@ -6,20 +6,20 @@ from migen.build.xilinx import XilinxPlatform
 _io = [
         ("clk100", 0, Pins("Y9"), IOStandard("LVCMOS33")),
 
-        ("user_btn", 0, Pins("P16"), IOStandard("LVCMOS18")),  # center
-        ("user_btn", 1, Pins("R16"), IOStandard("LVCMOS18")),  # down
-        ("user_btn", 2, Pins("N15"), IOStandard("LVCMOS18")),  # left
-        ("user_btn", 3, Pins("R18"), IOStandard("LVCMOS18")),  # right
-        ("user_btn", 4, Pins("T18"), IOStandard("LVCMOS18")),  # up
+        ("user_btn", 0, Pins("P16"), IOStandard("LVCMOS25")),  # center
+        ("user_btn", 1, Pins("R16"), IOStandard("LVCMOS25")),  # down
+        ("user_btn", 2, Pins("N15"), IOStandard("LVCMOS25")),  # left
+        ("user_btn", 3, Pins("R18"), IOStandard("LVCMOS25")),  # right
+        ("user_btn", 4, Pins("T18"), IOStandard("LVCMOS25")),  # up
 
-        ("user_sw", 0, Pins("F22"), IOStandard("LVCMOS18")),
-        ("user_sw", 1, Pins("G22"), IOStandard("LVCMOS18")),
-        ("user_sw", 2, Pins("H22"), IOStandard("LVCMOS18")),
-        ("user_sw", 3, Pins("F21"), IOStandard("LVCMOS18")),
-        ("user_sw", 4, Pins("H19"), IOStandard("LVCMOS18")),
-        ("user_sw", 5, Pins("H18"), IOStandard("LVCMOS18")),
-        ("user_sw", 6, Pins("H17"), IOStandard("LVCMOS18")),
-        ("user_sw", 7, Pins("M15"), IOStandard("LVCMOS18")),
+        ("user_sw", 0, Pins("F22"), IOStandard("LVCMOS25")),
+        ("user_sw", 1, Pins("G22"), IOStandard("LVCMOS25")),
+        ("user_sw", 2, Pins("H22"), IOStandard("LVCMOS25")),
+        ("user_sw", 3, Pins("F21"), IOStandard("LVCMOS25")),
+        ("user_sw", 4, Pins("H19"), IOStandard("LVCMOS25")),
+        ("user_sw", 5, Pins("H18"), IOStandard("LVCMOS25")),
+        ("user_sw", 6, Pins("H17"), IOStandard("LVCMOS25")),
+        ("user_sw", 7, Pins("M15"), IOStandard("LVCMOS25")),
 
         ("user_led", 0, Pins("T22"), IOStandard("LVCMOS33")),
         ("user_led", 1, Pins("T21"), IOStandard("LVCMOS33")),
@@ -106,34 +106,92 @@ _io = [
             Subsignal("ad8_n", Pins("D17")),
             Subsignal("ad8_p", Pins("D16")),
             IOStandard("LVCMOS18")),
+]
 
-        ("fmc_clocks", 0,
-            Subsignal("clk0_n", Pins("L19")),
-            Subsignal("clk0_p", Pins("L18")),
-            Subsignal("clk1_n", Pins("C19")),
-            Subsignal("clk1_p", Pins("D18")),
-            IOStandard("LVCMOS18")),
 
-        ("fmc", 0,
-            Subsignal("scl", Pins("R7")),
-            Subsignal("sda", Pins("U7")),
-
-            Subsignal("prsnt", Pins("AB14")),
-
-            # 0, 1, 17, 18 can be clock signals
-            Subsignal("la_n", Pins(
-                "M20 N20 P18 P22 M22 K18 L22 T17 "
-                "J22 R21 T19 N18 P21 M17 K20 J17 "
-                "K21 B20 C20 G16 G21 E20 F19 D15 "
-                "A19 C22 E18 D21 A17 C18 B15 B17 "
-                "A22 B22")),
-            Subsignal("la_p", Pins(
-                "M19 N19 P17 N22 M21 J18 L21 T16 "
-                "J21 R20 R19 N17 P20 L17 K19 J16 "
-                "J20 B19 D20 G15 G20 E19 G19 E15 "
-                "A18 D22 F18 E21 A16 C17 C15 B16 "
-                "A21 B21")),
-            IOStandard("LVCMOS18")),
+_connectors = [
+    ("fmc", {
+        "LA33_N": "B22",
+        "LA32_N": "A22",
+        "LA31_N": "B17",
+        "LA30_N": "B15",
+        "LA29_N": "C18",
+        "LA28_N": "A17",
+        "LA27_N": "D21",
+        "LA26_N": "E18",
+        "LA25_N": "C22",
+        "LA24_N": "A19",
+        "LA23_N": "D15",
+        "LA22_N": "F19",
+        "LA21_N": "E20",
+        "LA20_N": "G21",
+        "LA19_N": "G16",
+        "LA18_CC_N": "C20",
+        "LA17_CC_N": "B20",
+        "LA16_N": "K21",
+        "LA15_N": "J17",
+        "LA14_N": "K20",
+        "LA13_N": "M17",
+        "LA12_N": "P21",
+        "LA11_N": "N18",
+        "LA10_N": "T19",
+        "LA09_N": "R21",
+        "LA08_N": "J22",
+        "LA07_N": "T17",
+        "LA06_N": "L22",
+        "LA05_N": "K18",
+        "LA04_N": "M22",
+        "LA03_N": "P22",
+        "LA02_N": "P18",
+        "LA01_CC_N": "N20",
+        "LA00_CC_N": "M20",
+        "LA33_P": "B21",
+        "LA32_P": "A21",
+        "LA31_P": "B16",
+        "LA30_P": "C15",
+        "LA29_P": "C17",
+        "LA28_P": "A16",
+        "LA27_P": "E21",
+        "LA26_P": "F18",
+        "LA25_P": "D22",
+        "LA24_P": "A18",
+        "LA23_P": "E15",
+        "LA22_P": "G19",
+        "LA21_P": "E19",
+        "LA20_P": "G20",
+        "LA19_P": "G15",
+        "LA18_CC_P": "D20",
+        "LA17_CC_P": "B19",
+        "LA16_P": "J20",
+        "LA15_P": "J16",
+        "LA14_P": "K19",
+        "LA13_P": "L17",
+        "LA12_P": "P20",
+        "LA11_P": "N17",
+        "LA10_P": "R19",
+        "LA09_P": "R20",
+        "LA08_P": "J21",
+        "LA07_P": "T16",
+        "LA06_P": "L21",
+        "LA05_P": "J18",
+        "LA04_P": "M21",
+        "LA03_P": "N22",
+        "LA02_P": "P17",
+        "LA01_CC_P": "N19",
+        "LA00_CC_P": "M19",
+        # LVDS
+        "CLK0_M2C_N": "L19",
+        "CLK0_M2C_P": "L18",
+        "CLK1_M2C_N": "C19",
+        "CLK1_M2C_P": "D18",
+        # DIFF_HSTL_I_DCI_18
+        "GBTCLK0_M2C_P": "",
+        "GBTCLK0_M2C_N": "",
+        "DP0_M2C_P": "",
+        "DP0_M2C_N": "",
+        "DP0_C2M_P": "",
+        "DP0_C2M_N": "",
+    }),
 ]
 
 
@@ -142,4 +200,5 @@ class Platform(XilinxPlatform):
     default_clk_period = 10
 
     def __init__(self):
-        XilinxPlatform.__init__(self, "xc7z020-clg484-1", _io)
+        XilinxPlatform.__init__(self, "xc7z020-clg484-1", _io, _connectors,
+                toolchain="vivado")
